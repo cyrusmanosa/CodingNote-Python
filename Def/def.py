6cde9a38fae7383c347fd6bfb2cb3a9c23ebaa14
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2025-05-07 22:37:13 +0800

    update

--- Def/def.py
+++ Def/def.py
@@ -13,3 +13,13 @@ def postTaxPrice2(price,name):
     return ans
 
 print(postTaxPrice2(name="apple",price=100))
+
+print("\n--------------- def (function) 3 -----------------")
+
+def multiply(*numbers):
+    total = 1
+    for number in numbers: 
+        total *= number
+    return total
+    
+print(multiply(2,3,4,5))
--- If/if.py
+++ If/if.py
@@ -5,3 +5,23 @@ if x <= 1:
     print("xは1以下です")
 else:
     print("xは1以上です")
+
+y = 22
+if y >= 10 and y <= 20:
+    print("yは10から20の間です")
+elif y < 10:
+    print("yは10未満です")
+
+print("Done!")
+
+age = 12
+msg = "Eligible" if age >= 18 else "Not Eligible"
+print(msg)
+
+if 790 > age >= 6:
+    print("Yes")
+
+
+abcd = "asdasdasd"
+if not abcd:
+    print("abcdは空です")
